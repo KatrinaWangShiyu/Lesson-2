bacae9675369d97b4f939365866b7ba7aa029624
王食欲 <59759021+KatrinaWangShiyu@users.noreply.github.com>
2020-01-17 10:11:09 +0800

    first commit

---
+++ 03.py
@@ -0,0 +1,46 @@
+'''
+int float string bool
+for
+if else
+
+# while
+
+i = 0
+while i < 5:
+
+    print(i)
+    i = i + 1
+
+
+
+# 函数
+
+def  function01(x):
+    x = x + 1
+    x = x / 5
+    return x
+
+
+i = 5
+i = function01(i)
+print(i)
+
+
+j = 7
+j = function01(j)
+print(j)
+'''
+
+
+import  need2import as cat
+import  numpy as np
+
+
+
+i = 7
+
+i = cat.function01(7)
+print(i)
+j = 8
+j  = cat.function02(j)
+print(j)
---
+++ need2import.py
@@ -0,0 +1,52 @@
+'''
+int float string bool
+for
+if else
+
+# while
+
+i = 0
+while i < 5:
+
+    print(i)
+    i = i + 1
+
+
+
+# 函数
+
+def  function01(x):
+    x = x + 1
+    x = x / 5
+    return x
+
+
+i = 5
+i = function01(i)
+print(i)
+
+
+j = 7
+j = function01(j)
+print(j)
+'''
+
+import  numpy as np
+
+
+
+
+def function01(x):
+    x = x + 1
+    x = x / 5
+    return x
+
+def function02(x):
+    x = x + 1
+    x = x / 5
+    return x
+
+def function03(x):
+    x = x + 1
+    x = x / 5
+    return x
